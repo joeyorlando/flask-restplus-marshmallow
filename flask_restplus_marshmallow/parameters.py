fdaf5446f28d6aa50afcd096953aa760152de836
Joey Orlando <joseph.t.orlando@gmail.com>
2018-01-18 14:40:23 -0500

    fix something

--- flask_restplus_marshmallow/parameters.py
+++ flask_restplus_marshmallow/parameters.py
@@ -6,14 +6,33 @@ class Parameters(Schema):
 
     LOCATION = 'json'
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    class Meta:
+        ordered = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        for required_field_name in getattr(self.Meta, 'required', []):
+            self.fields[required_field_name].required = True
 
         for field in itervalues(self.fields):
             if field.dump_only:
                 continue
             if not field.metadata.get('location'):
                 field.metadata['location'] = self.LOCATION
+
+    def __contains__(self, field):
+        return field in self.fields
+
+    def make_instance(self, data):
+        # pylint: disable=unused-argument
+        """
+        This is a no-op function which shadows ``ModelSchema.make_instance``
+        method (when inherited classes inherit from ``ModelSchema``). Thus, we
+        avoid a new instance creation because it is undesirable behaviour for
+        parameters (they can be used not only for saving new instances).
+        """
+        return
 
 class JSONParameters(Parameters):
 
