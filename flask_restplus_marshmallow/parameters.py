db451b25d8064b19760369d2e257fa4bd6cfa33d
Joey Orlando <joseph.t.orlando@gmail.com>
2018-01-18 13:46:03 -0500

    minor tweak

--- flask_restplus_marshmallow/parameters.py
+++ flask_restplus_marshmallow/parameters.py
@@ -6,24 +6,22 @@ class JSONParameters(Schema):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.location = None
+        self.location = 'json'
 
         for field in itervalues(self.fields):
             if field.dump_only:
                 continue
             if not field.metadata.get('location'):
-                self.location = 'json'
                 field.metadata['location'] = 'json'
 
 class QueryParameters(Schema):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.location = None
+        self.location = 'query'
 
         for field in itervalues(self.fields):
             if field.dump_only:
                 continue
             if not field.metadata.get('location'):
-                self.location = 'query'
                 field.metadata['location'] = 'query'
