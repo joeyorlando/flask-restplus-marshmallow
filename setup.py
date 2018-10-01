08b9bd10c9805e3dd029de0dc6b21f5c94f61baa
Joey Orlando <joseph.t.orlando@gmail.com>
2018-10-01 15:29:37 -0400

    fix syntax error

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email="joey@100danish.com",
     description="Flask RESTPlus with a twist of marshmallow",
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     packages=[
         'flask_restplus_marshmallow'
     ],
